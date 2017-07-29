22f01e5936e25fca04d0ca0f1cb4f7be59c97922
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-07-29 17:11:55 +0100

    fix travis

--- server/models/documents.js
+++ server/models/documents.js
@@ -23,7 +23,6 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Document.associate = (models) => {
-    console.log(models)
     Document.belongsTo(models.User, {
       foreignKey: 'userId',
       onDelete: 'CASCADE',
--- server/models/index.js
+++ server/models/index.js
@@ -7,7 +7,6 @@ const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
-console.log(config);
 
 const db = {};
 dotenv.config();
