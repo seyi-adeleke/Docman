d62be35bd22c6de921685176de5f0f76c0e75924
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-08-04 12:09:46 +0100

    Refactor:
    Users should be able to get a list of all publicly available documents

--- server/controllers/documentControllers.js
+++ server/controllers/documentControllers.js
@@ -1,6 +1,13 @@
-import paginate from '../utilities/paginate';
+import validator from 'validator';
+
+import db from '../models/index';
+
+
+import utilities from '../utilities/paginate';
+import date from '../utilities/formatDate';
 
 const Document = require('../models').Document;
+const User = require('../models').User;
 
 export default {
   /**
@@ -12,7 +19,8 @@ export default {
    */
   create: (req, res) => {
     if (req.body.title === undefined || req.body.content === undefined) {
-      return res.status(400).send({ message: 'Please input a title or some content' });
+      return res.status(400)
+        .send({ message: 'Please input a title or some content' });
     }
     const body = req.body;
     if (body.access === undefined) {
@@ -23,7 +31,8 @@ export default {
     body.roleId = req.decoded.roleId;
     if (body.roleId === 2) {
       if (body.access === 'role') {
-        return res.status(404).json({ message: 'You cannot create Role based documents' });
+        return res.status(404)
+          .json({ message: 'You cannot create Role based documents' });
       }
     }
     Document
@@ -33,11 +42,14 @@ export default {
         document: {
           title: response.title,
           content: response.content,
-          access: response.access
+          access: response.access,
+          createdOn: response.createdAt,
+          id: response.id,
         }
       }))
       .catch(error => res.status(400).send(error));
   },
+
   /**
     *list: This lists out all documents in the database
     * @function list
@@ -46,16 +58,43 @@ export default {
     * @return {object} Json data 
     */
   list: (req, res) => {
-    const query = {
-      where: {}
-    };
+    let query;
+    if (req.decoded.roleId === 2) {
+      query = {
+        where: { access: 'public' },
+        include: [{
+          model: db.User,
+          attributes: ['name', 'email', 'id']
+        }],
+        attributes: ['title', 'content', 'access', 'createdAt', 'id']
+      };
+    } else if (req.decoded.roleId === 3) {
+      query = {
+        include: [{
+          model: db.User,
+          attributes: ['name', 'email', 'id']
+        }],
+        where: { $or: [{ access: 'public' }, { access: 'role' }] },
+        attributes: ['title', 'content', 'access', 'createdAt', 'id']
+      };
+    } else {
+      query = {
+        where: {},
+        include: [{
+          model: db.User,
+          attributes: ['name', 'email', 'id']
+        }],
+        attributes: ['title', 'content', 'access', 'createdAt', 'id']
+
+      };
+    }
     query.limit = Math.abs(req.query.limit) || 10;
     query.offset = Math.abs(req.query.offset) || 0;
     Document
       .findAll(query)
       .then((documents) => {
         const count = documents.length;
-        paginate.information(count, query.limit, query.offset, documents, res);
+        utilities.paginate(count, query.limit, query.offset, documents, res);
       })
       .catch(error => res.status(400).send(error));
   },
@@ -68,40 +107,70 @@ export default {
    * @return {object} Json data
    */
   findDocument: (req, res) => {
+    if (validator.isAlpha(req.params.id)) {
+      return res.status(400)
+        .send({ message: 'Please use an integer value' });
+    }
     Document
-      .findById(req.params.id)
+      .findById(req.params.id, {
+        include: [{
+          model: User,
+          as: 'User',
+          attributes: ['name', 'email']
+        }],
+      })
       .then((document) => {
         if (!document) {
-          return res.status(404).json({ message: 'This document doesn\'t exist' });
+          return res.status(404)
+            .json({ message: 'This document doesn\'t exist' });
         }
         const foundDocument = {
+          owner: document.User.dataValues.name,
           title: document.title,
           content: document.content,
-          access: document.access
+          access: document.access,
+          createdOn: (document.createdAt),
+          id: document.id,
         };
+
         if (req.isAdmin) {
-          return res.status(200).send(document);
+          return res.status(200).json({
+            message: 'Document Found succesfully', foundDocument
+          });
         } else if (document.access === 'public') {
-          return res.status(200).json({ message: 'Document Found succesfully', foundDocument });
-        } else if (document.access === 'private' && req.decoded.id !== document.userId) {
-          return res.status(404).json({ message: 'You do not have access to this document' });
+          return res.status(200).json({
+            message: 'Document Found succesfully', foundDocument
+          });
+        } else if (document.access === 'private'
+          && req.decoded.id !== document.userId) {
+          return res.status(400).json({
+            message: 'You do not have access to this document'
+          });
         } else if (document.access === 'role' && req.decoded.roleId === 3) {
-          return res.status(200).json({ message: 'Document Found succesfully', foundDocument });
+          return res.status(200).json({
+            message: 'Document Found succesfully', foundDocument
+          });
         }
       })
       .catch(error => res.status(400).send(error));
   },
 
-/**
-  * updateDocument: This updates a document
-  * @function updateDocument
-  * @param {object} req
-  * @param {object} res
-  * @return {object} Json data
-*/
+  /** 
+    * updateDocument: This updates a document
+    * @function updateDocument
+    * @param {object} req
+    * @param {object} res
+    * @return {object} Json data
+   */
   updateDocument: (req, res) => {
-    if (req.body.title === undefined && req.body.content === undefined && req.body.access === undefined) {
-      return res.status(400).send({ message: 'Please cross check your request' });
+    if (validator.isAlpha(req.params.id)) {
+      return res.status(400).send({ message: 'Please use an integer value' });
+    }
+    if (req.body.title === undefined &&
+      req.body.content === undefined && req.body.access === undefined) {
+      return res.status(400).send({
+        message: 'Please cross check your request'
+      });
     }
     Document
       .findById(req.params.id)
@@ -109,15 +178,31 @@ export default {
         const updatedDocument = {
           title: req.body.title,
           content: req.body.content,
-          access: req.body.access
+          access: req.body.access,
+          updatedOn: date.format(document.updatedAt)
         };
-        if (req.body.access === 'role' && req.decoded.roleId === 2) {
-          return res.status(400).send({ message: 'You cannot create role based documents' });
+        if (req.body.access) {
+          if (req.body.access !== 'private' &&
+            req.body.access !== 'public' && req.body.access !== 'role') {
+            return res.status(400).send({
+              document: 'You cannot change a document to this access level'
+            });
+          }
+        }
+        if (req.body.access === 'role'
+          && req.decoded.roleId === 2) {
+          return res.status(400).send({
+            message: 'You cannot create role based documents'
+          });
         }
         if (!document) {
-          return res.status(404).json({ message: 'this document doesnt exist' });
+          return res.status(404).json({
+            message: 'this document doesnt exist'
+          });
         } else if (document.userId !== req.decoded.id) {
-          return res.status(404).json({ message: 'you cannot edit this document' });
+          return res.status(404).json({
+            message: 'you cannot edit this document'
+          });
         }
         return document
           .update({
@@ -125,42 +210,53 @@ export default {
             content: req.body.content || document.content,
             access: req.body.access || document.access
           })
-          .then(() => res.status(200).json({ message: 'Document updated', updatedDocument }));
+          .then(() => res.status(200).json({
+            message: 'Document updated', updatedDocument
+          }));
       })
       .catch(error => res.status(400).send(error));
   },
 
-/**
-   * deleteDocument: This deletes a users documents
-   * @function deleteDocument
-   * @param {object} req
-   * @param {object} res
-   * @return {null}
-   */
+  /**
+     * deleteDocument: This deletes a users documents
+     * @function deleteDocument
+     * @param {object} req
+     * @param {object} res
+     * @return {null} void
+    */
   deleteDocument: (req, res) => {
+    if (validator.isAlpha(req.params.id)) {
+      return res.status(400).send({ message: 'Please use an integer value' });
+    }
     Document
       .findById(req.params.id)
       .then((document) => {
         if (!document) {
-          return res.status(404).json({ message: 'this document doesnt exist' });
+          return res.status(404).json({
+            message: 'this document doesnt exist'
+          });
         } else if ((req.decoded.id === document.userId || req.isAdmin)) {
           return document
             .destroy()
-            .then(() => res.status(204).send())
+            .then(() => res
+              .json({ message: 'Document has been deleted successfully' }))
             .catch(error => res.status(400).send(error));
         } else if (req.decoded.id !== document.userId) {
-          return res.status(404).json({ message: 'you dont have access to this document' });
+          return res.status(404).json({
+            message: 'you dont have access to this document'
+          });
         }
       })
       .catch(error => res.status(400).send(error));
   },
-/**
- * searchDocument: This searches for a particular document
- * @function searchDocuments
- * @param {object} req
- * @param {object} res
- * @return {object} Json data
- */
+
+  /**
+   * searchDocument: This searches for a particular document
+   * @function searchDocuments
+   * @param {object} req
+   * @param {object} res
+   * @return {object} Json data
+   */
   searchDocuments: (req, res) => {
     let query;
     if (req.isAdmin) {
@@ -173,7 +269,10 @@ export default {
       };
     } else if (req.decoded.roleId === 3) {
       query = {
-        where: { title: req.query.q, $or: [{ access: 'public' }, { access: 'role' }] }
+        where: {
+          title: req.query.q,
+          $or: [{ access: 'public' }, { access: 'role' }]
+        }
       };
     }
     Document
@@ -181,7 +280,8 @@ export default {
       .then((document) => {
         const message = 'Document Found';
         if (document[0] === undefined) {
-          return res.status(404).json({ message: 'This document doesnt exist' });
+          return res.status(404)
+            .json({ message: 'This document doesnt exist' });
         }
         const searchedDocument = {
           title: document[0].dataValues.title,
@@ -191,6 +291,5 @@ export default {
       })
       .catch(error => res.status(400).send(error));
   }
-
 };
 
