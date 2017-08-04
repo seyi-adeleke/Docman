e66c15aaa74a161989aff197e6162ba402caba91
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-08-04 11:57:58 +0100

    Refactor(linter):
    Code was refactored to maintain a proper max length of less than 80 characters

--- server/routes.js
+++ server/routes.js
@@ -4,26 +4,49 @@ import documentsController from './controllers/documentControllers';
 import verify from './utilities/verifyUser';
 
 module.exports = (app) => {
-  app.post('/api/v1/users', usersController.signUp);
+  app.post('/api/v1/users',
+    usersController.signUp);
 
-  app.post('/api/v1/users/login', usersController.login);
-  app.get('/api/v1/users', verify.isAdmin, usersController.allUsers);
+  app.post('/api/v1/users/login',
+    usersController.login);
 
-  app.get('/api/v1/users/:id', verify.isLoggedIn, usersController.getUser);
-  app.put('/api/v1/users/:id', verify.isLoggedIn, usersController.updateUser);
-  app.delete('/api/v1/users/:id', verify.isAdmin, usersController.deleteUser);
-  app.get('/api/v1/users/:id/documents', verify.isLoggedIn, usersController.searchUserDocuments);
+  app.get('/api/v1/users',
+    verify.isAdmin, usersController.getAllUsers);
 
-  app.post('/api/v1/documents', verify.isLoggedIn, documentsController.create);
+  app.get('/api/v1/users/:id',
+    verify.isLoggedIn, usersController.getUser);
 
-  app.get('/api/v1/documents', verify.isAdmin, documentsController.list);
-  app.get('/api/v1/documents/:id', verify.isLoggedIn, documentsController.findDocument);
-  app.put('/api/v1/documents/:id', verify.isLoggedIn, documentsController.updateDocument);
-  app.delete('/api/v1/documents/:id', verify.isLoggedIn, documentsController.deleteDocument);
+  app.put('/api/v1/users/:id',
+    verify.isLoggedIn, usersController.updateUser);
 
-  app.get('/api/v1/search/documents/', verify.isLoggedIn, documentsController.searchDocuments);
-  app.get('/api/v1/search/users/', verify.isLoggedIn, usersController.searchUsers);
+  app.delete('/api/v1/users/:id',
+    verify.isAdmin, usersController.deleteUser);
 
-  app.put('/api/v1/users/:id/role', verify.isAdmin, usersController.changeRole);
+  app.get('/api/v1/users/:id/documents',
+    verify.isLoggedIn, usersController.searchUserDocuments);
+
+  app.post('/api/v1/documents',
+    verify.isLoggedIn, documentsController.create);
+
+  app.get('/api/v1/documents',
+    verify.isLoggedIn, documentsController.list);
+
+  app.get('/api/v1/documents/:id',
+    verify.isLoggedIn, documentsController.findDocument);
+
+  app.put('/api/v1/documents/:id',
+    verify.isLoggedIn, documentsController.updateDocument);
+
+  app.delete('/api/v1/documents/:id',
+    verify.isLoggedIn, documentsController.deleteDocument);
+
+  app.get('/api/v1/search/documents/',
+    verify.isLoggedIn, documentsController.searchDocuments);
+
+  app.get('/api/v1/search/users/',
+    verify.isLoggedIn, usersController.searchUsers);
+
+  app.put('/api/v1/users/:id/role',
+    verify.isAdmin, usersController.changeRole);
 };
 
