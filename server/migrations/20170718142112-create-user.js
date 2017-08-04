661fe2aedc3dd717a5e12a3e888388b8beb52503
seyi-adeleke <adetokunbo.adeleke@andela.com>
2017-08-04 12:07:16 +0100

    Refactor(dates):
    Dates should be saved in an easily readable format

--- server/migrations/20170718142112-create-user.js
+++ server/migrations/20170718142112-create-user.js
@@ -27,11 +27,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       }
     });
   },
--- server/migrations/20170720170746-create-documents.js
+++ server/migrations/20170720170746-create-documents.js
@@ -39,11 +39,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       }
     });
   },
